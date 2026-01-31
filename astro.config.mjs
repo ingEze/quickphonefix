0c86a27a543bc11694774a379cbaf71df423557f
Ezequiel <ezequielrodrigosaucedo@gmail.com>
2026-01-31 18:01:41 -0300

    finish

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import react from '@astrojs/react';
+import { defineConfig } from "astro/config"
+import react from "@astrojs/react"
+import vercel from "@astrojs/vercel/serverless"
 
 export default defineConfig({
-  integrations: [react()],
-  vite: {
-    server: {
-      host: true,
-      allowedHosts: ['.ngrok-free.dev']
-    },
-  },
+  output: "server",
+  adapter: vercel({}),
+  integrations: [react()]
 })
